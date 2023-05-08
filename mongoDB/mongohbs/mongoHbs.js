2ac70db45efa1b2661bd8ea7243694d52cb4bf8e
Yurii <iyureg@gmail.com>
2023-05-08 23:27:13 +0300

    add mongohbs dir

---
+++ mongoDB/mongohbs/mongoHbs.js
@@ -0,0 +1,52 @@
+import expressHandlebars from "express-handlebars";
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const handlebars = expressHandlebars.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+let app = express();
+app.engine("hbs", handlebars.engine);
+app.set("view engine", "hbs");
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri, {
+  useUnifiedTopology: true,
+});
+client.connect(async (error, mongo) => {
+  if (!error) {
+    console.log("connected mongo");
+  } else {
+    console.log(error);
+  }
+});
+
+// --- findOne ---
+app.get("/user/show/:name", async function (req, res) {
+  /*
+    http://localhost:3000/user/show/user1
+                                    user2
+                                    user3
+ */
+  let name = req.params.name;
+  let user = await client
+    .db("test")
+    .collection("users")
+    .findOne({ name: name });
+
+  if (user == null) {
+    let error = "not found";
+    res.render("404", { data: error });
+  } else {
+    res.render("user", {
+      name: user.name,
+      age: user.age,
+      salary: user.salary,
+    });
+  }
+});
+
+app.use((req, res) => res.status(404).render("404"));
+app.listen(3000, () => console.log("localhost:3000 running..."));
