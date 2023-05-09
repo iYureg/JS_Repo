9d2ed9b6453f8be19e8608be2acb4741ce0a3e83
Yurii <iyureg@gmail.com>
2023-05-09 03:58:56 +0300

    adding documents form

--- mongoDB/mongohbs/mongoHbs.js
+++ mongoDB/mongohbs/mongoHbs.js
@@ -1,6 +1,7 @@
 import expressHandlebars from "express-handlebars";
 import express from "express";
 import { MongoClient } from "mongodb";
+import bodyParser from "body-parser";
 
 const handlebars = expressHandlebars.create({
   defaultLayout: "main",
@@ -10,6 +11,7 @@ const handlebars = expressHandlebars.create({
 let app = express();
 app.engine("hbs", handlebars.engine);
 app.set("view engine", "hbs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri, {
@@ -23,13 +25,24 @@ client.connect(async (error, mongo) => {
   }
 });
 
+// --------- insertOne User ----------
+app.get("/user/add", (req, res) => {
+  res.render("addUser");
+});
+app.post("/user/add", async (req, res) => {
+  let user = req.body;
+  await client.db("test").collection("users").insertOne(user);
+
+  res.render("message", { data: "user successfully added" });
+});
+
 // --- findOne ---
 app.get("/user/show/:name", async function (req, res) {
   /*
-    http://localhost:3000/user/show/user1
-                                    user2
-                                    user3
- */
+  http://localhost:3000/user/show/user1
+  user2
+  user3
+  */
   let name = req.params.name;
   let user = await client
     .db("test")
@@ -46,13 +59,32 @@ app.get("/users", async function (req, res) {
   let users = await client.db("test").collection("users").find().toArray();
   res.render("users", { users: users });
 });
+// --- delete user ---
+app.get("/user/delete/:name", async (req, res) => {
+  let name = req.params.name;
+  await client.db("test").collection("users").deleteOne({ name: name });
+  res.render("message", { data: "user successfully deleted" });
+});
 
+// ========= Products =========
+// --------- insertOne Prod ----------
+app.get("/prod/add", (req, res) => {
+  res.render("addProd");
+});
+app.post("/prod/add", async (req, res) => {
+  let prod = req.body;
+  await client.db("test").collection("prods").insertOne(prod);
+
+  res.render("message", { data: "prod successfully added" });
+});
+
+// ---- findOne prod -----
 app.get("/prod/show/:name", async function (req, res) {
   /*
     http://localhost:3000/prod/show/prod1
-                                    prod2
-                                    prod3
- */
+    prod2
+    prod3
+    */
   let name = req.params.name;
   let prod = await client
     .db("test")
@@ -72,12 +104,10 @@ app.get("/prods", async function (req, res) {
 // --- delete prod ---
 app.get("/prod/delete/:name", async (req, res) => {
   let name = req.params.name;
-  let prod = await client
-    .db("test")
-    .collection("prods")
-    .deleteOne({ name: name });
-  res.render("message", { data: "successfully deleted" });
+  await client.db("test").collection("prods").deleteOne({ name: name });
+  res.render("message", { data: "prod successfully deleted" });
 });
+
 app.use((req, res) =>
   res.status(404).render("404", { data: "page not found" })
 );
