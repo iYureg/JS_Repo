e921b05fb590e1893710e893c7a73056eab10c92
Yurii <iyureg@gmail.com>
2023-05-13 21:00:38 +0300

    modified MathComp

--- VueRepo/introVue/introVue/src/components/MathComp.vue
+++ VueRepo/introVue/introVue/src/components/MathComp.vue
@@ -4,13 +4,15 @@ export default {
     return {
       var1: 1,
       var2: 2,
+      num3: 3,
+      pow: 3,
     };
   },
   name: "MathComp",
 };
 </script>
 <template>
-  <div class="content">
+  <div class="math">
     {{ var1 }}<span class="red"> + </span> {{ var2 }} = {{ var1 + var2 }}
     <br />
     {{ var1 }}<span class="red"> - </span> {{ var2 }} = {{ var1 - var2 }}
@@ -18,6 +20,10 @@ export default {
     {{ var1 }}<span class="red"> * </span> {{ var2 }} = {{ var1 * var2 }}
     <br />
     {{ var1 }}<span class="red"> : </span> {{ var2 }} = {{ var1 / var2 }}
+    <br />
+    {{ num3 }} в квадрате = {{ num3 ** var2 }}
+    <br />
+    {{ num3 }} в кубе = {{ Math.pow(num3, pow) }}
   </div>
 </template>
 
@@ -25,7 +31,8 @@ export default {
 .red {
   color: red;
 }
-.content {
+.math {
+  margin: 10px;
   text-align: center;
 }
 </style>
