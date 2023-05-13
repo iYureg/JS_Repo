e2d2e70fb786b3ab02198b616cf58a8cf22b931b
Yurii <iyureg@gmail.com>
2023-05-14 01:11:10 +0300

    add button v-on click

--- VueRepo/introVue/introVue/src/components/HelloMethods.vue
+++ VueRepo/introVue/introVue/src/components/HelloMethods.vue
@@ -3,12 +3,37 @@ export default {
   data() {},
   methods: {
     now: () => new Date(),
+    showDate: () => {
+      let date = new Date();
+      alert(date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate());
+    },
+    showTime: () => {
+      let time = new Date();
+      let hour = "";
+      let min = "";
+      let sec = "";
+
+      time.getHours() < 10
+        ? (hour = "0" + time.getHours())
+        : (hour = time.getHours());
+      time.getMinutes() < 10
+        ? (min = "0" + time.getMinutes())
+        : (min = time.getMinutes());
+      time.getSeconds() < 10
+        ? (sec = "0" + time.getSeconds())
+        : (sec = time.getSeconds());
+      alert(hour + ":" + min + ":" + sec);
+    },
   },
   name: "HelloMethods",
 };
 </script>
 <template>
   <div class="date">
+    <button v-on:click="showDate">showDate</button>
+    <br />
+    <button @click="showTime">showTime</button>
+    <br />
     Дата: {{ now().getFullYear() }}-{{
       now().getMonth() < 10
         ? "0" + (now().getMonth() + 1)
